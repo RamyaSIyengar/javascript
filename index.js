3fcc716a838b1c6dd846591ba4574fbbaa49a8d1
Ramya <ramyaiyengar12@gmail.com>
2023-10-28 15:04:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// everything in Js happens inside an execution context
+// Hoisting in js means you can access variables and func before even intialized it
+// getName();
+// console.log(x);
+// console.log(getName); // 
+
+// var x=7;
+
+// function getName(){
+//       console.log("javascript");
+// }
+
+// diff ways of writing function
+//Arrow function
+getName();
+
+var getName = () => {    // treated as variable -undefined 
+  console.log("namaste");
+}
+
+var getName2 = function(){
+
+}
+
+
+
+
+
