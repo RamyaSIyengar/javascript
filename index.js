2d1c572453fbc1d743794e43333c7673df371644
Ramya <ramyaiyengar12@gmail.com>
2023-11-02 07:31:42 -0400

    changes2

--- index.js
+++ index.js
@@ -13,19 +13,58 @@
 
 // diff ways of writing function
 //Arrow function
-getName();
+// getName();
 
-var getName = () => {    // treated as variable -undefined 
-  console.log("namaste");
+// var getName = () => {    // treated as variable -undefined 
+//   console.log("namaste");
+// }
+
+// var getName2 = function(){
+
+// }
+
+// let b=7
+// console.log(typeof(b))
+
+
+// Rules and convention  of var => letter,numbers,_, $
+var $nation ="India";
+console.log($nation);
+
+var firstName = "Krishna"; //camelcase
+console.log(firstName);
+
+var first_name = "Ram"; //underscore
+console.log(first_name);
+
+var FirstName = "Sham"; //pascal case
+console.log(FirstName);
+
+// let firstName = "KrishnaBhagvan"; //camelcase
+// console.log(firstName);
+//Uncaught SyntaxError: Identifier 'firstName' has already been declared (at index.js:40:5)
+
+let lastName = "KrishnaBhagvan"; //camelcase
+console.log(lastName);
+
+const fullName = 'RamyaIyengar';
+console.log(fullName);
+// fullName='RashmiIyengar';
+// console.log(fullName);
+//index.js:49 Uncaught TypeError: Assignment to constant variable.
+
+const people = {
+    name: "Ramya"
 }
+people.name = "Rash";
+// we are changing the value of object inside the constant, not the constant value
+console.log(people);
 
-var getName2 = function(){
+// ARRAY
 
-}
-
-let b=7
-console.log(typeof(b))
-
-
+const numbers = [1,2,3,4,5];
+// numbers = [1,2,3,4,5,6]; Uncaught TypeError: Assignment to constant variable.
+numbers.push(6); // you can manipulate values in the array obj 
+console.log(numbers);
 
 
