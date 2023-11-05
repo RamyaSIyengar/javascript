9e8b36605e33aa2a7d09bdd226628c7480284705
Ramya <ramyaiyengar12@gmail.com>
2023-11-05 15:30:02 -0500

    new changes

--- index.js
+++ index.js
@@ -79,4 +79,90 @@ Arrays, Object Literals, Functions
 */
 
 const hisName = 'Rajesh';
-console.log(typeof hisName);
+const age=25;
+const isMale=true;
+const abc = null; // storing value of nothing
+let a; // if we dont intialize with a value then it is undefined
+const sym = Symbol();
+console.log(hisName, age, isMale, abc, a ,sym);
+
+//reference datatype
+//Arrays
+const hobbies=["singing","drawing"]
+//object literals
+const person={
+    name:'Ram',
+    age:25,
+    job:'it'
+}
+
+const today = new Date();
+
+console.log(hobbies, person, today)
+
+//Type conversion and cohesion
+
+let value;
+//number to a string
+
+value = 5;
+value = String(5*5);
+
+//boolean to string
+value = String(true)
+
+//date to string
+value = String(new Date());
+
+//array to string
+value = String([1,2,3]);
+
+//toString method to do the conversion
+value =(5).toString();
+value= (true).toString();
+
+// console.log(value); 
+// console.log(typeof value);
+// console.log(value.length) for string
+
+/*
+other types to Number
+*/
+value =Number("5");
+value =Number(false);
+value =Number(null);
+
+//parseInt() method to covrert any type to number
+value= parseInt('5');
+
+console.log(value); 
+console.log(typeof value);
+console.log(value.toFixed(2)) //length is a property of a string which gives no of characters in string it gives undefined for number
+
+//Type coehesion - JS will convert it for us
+console.log("Type coehesion")
+const value1 = '5';
+const value2 = 3;
+const sum = value1+value2;
+console.log(sum);
+console.log(typeof sum)
+
+//template literals(es6)
+let html;
+const firstName='ramya';
+const lName = 'iyengar';
+
+
+
+html = `
+<ul>
+    <li>Firstname:${firstName}</li>
+    <li>lastname:${lName}</li>
+
+</ul>
+`
+document.body.innerHTML=html;
+
+
+
+
