fda59bcafbe9dd5b778a9be91927fb5ed5c5da06
Ramya <ramyaiyengar12@gmail.com>
2023-11-10 06:17:54 -0500

    changes

--- app.js
+++ app.js
@@ -13,3 +13,38 @@ x(function y(){
 })
 //if you call a function and pass a function inside another function then that function is called callback function
 
+  
+
+//Blocking the main thread - everything is executed in a page is through call stack, if any operation blocks the call stack is called blocking the main thread
+
+//Event Listeners
+
+//added click me button in html 
+/*document.getElementById("clickMe")
+.addEventListener("click",function clickfunc(){
+        console.log("button clicked");
+}); // function here is called callback function*/
+
+//add count to check how many times the button is clicked
+
+/*
+let count=0;
+document.getElementById("clickMe").addEventListener("click",function clickfunc(){
+    console.log("button clicked",++count)
+});
+*/
+
+//using a global variable is not a good practice so the count is not modified by any other program
+//lets add closure to eventlistners
+//wrap the above code in a function 
+
+function attachEventListeners(){
+    let count=0;
+    document.getElementById("clickMe").addEventListener("click",function clickfunc(){
+        console.log("button clicked",++count)
+    });
+}
+attachEventListeners();
+
+//Garbage Collection and remove eventListeners
+// eventlisterners take memry - which doesnt freeup by itself
